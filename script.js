ee8703fb9cb6c4fbd667c222e55cda596ad25c6e
Mikael Dúi Bolinder <github@bolinder.uk>
2025-06-11 21:58:51 +0300

    Remove exit IPs

--- script.js
+++ script.js
@@ -22,37 +22,9 @@ $(document).ready( function () {
                 logical.Domain,
                 logical.Name,
                 logical.Servers[0].EntryIP,
-                logical.Servers[0].ExitIP,
                 nodesIpv6[logical.Domain] || (!!(16 & logical.Features) ? MSG_UNKNOWN_IPV6 : ""), // Entry IPv6
-                "", // Exit IPv6
                 isp ? isp.org : ""
             ])
-        });
-
-        // Guess Exit IPv6 Addresses
-        var hasIpv6 = data.filter(l => l[4] != "" && l[4] != MSG_UNKNOWN_IPV6);
-
-        hasIpv6.sort((a, b) => Number(a[1].split("#")[1]) -Number(b[1].split("#")[1]));
-
-        Object.entries(Object.groupBy(hasIpv6, d => d[0])).forEach(
-            ([_, node]) =>  {
-            var prefix = node[0][4].split("::")[0];
-            var suffixInt = parseInt(node[0][4].split("::")[1], 16);
-
-            // Fix for co-01. Guess AAAA record has the wrong IP?
-            if (suffixInt == 17) {
-                suffixInt = 16;
-            }
-
-            // Fixes it for some servers on node-ch-15. It's a mess.
-            if (suffixInt == 13) {
-                suffixInt = 32;
-            }
-            
-            node.forEach(server => {
-                suffixInt++;
-                server[5] = prefix + "::" + suffixInt.toString(16);
-            });
         });
 
         var table = $('#table').DataTable({
